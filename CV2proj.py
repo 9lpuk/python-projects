243c3a70a790b8dc38923014415b768e5d36d2b5
9lpuk <9lpuk.ko@gmail.com>
2025-12-27 10:16:20 +0400

    update

--- CV2proj.py
+++ CV2proj.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
-def imshow(img):             # img - cv2.imread('путь к файлу')
+def imshow(img,window_name):             # img - cv2.imread('путь к файлу')
     if img is None:
         print('error')
     else:
-        cv2.imshow('image',img)
+        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+        cv2.imshow(window_name,img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-#image =  cv2.imread('/home/yarik/Рабочий стол/Python/Git/img.jpeg')
+image =  cv2.imread('/home/yarik/Рабочий стол/Python/Git/img.jpeg')
+window_name = 'window1'
+imshow(image,window_name)
